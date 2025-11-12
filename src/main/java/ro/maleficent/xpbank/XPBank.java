5b6f459fa9bc5c9f404e7030d2d6c5749bafa6fa
Adrian Mihu <adrian.mihu20@icloud.com>
2025-11-12 18:59:59 +0200

    Initial Fabric mod template creation

---
+++ src/main/java/ro/maleficent/xpbank/XPBank.java
@@ -0,0 +1,17 @@
+package ro.maleficent.xpbank;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class XPBank implements ModInitializer {
+	public static final String MOD_ID = "xp-bank";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/ro/maleficent/xpbank/XPBankClient.java
@@ -0,0 +1,10 @@
+package ro.maleficent.xpbank;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class XPBankClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
