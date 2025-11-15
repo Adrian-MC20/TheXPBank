068b667ae2fcce94a8018c2e16f9016cd2123c23
Adrian Mihu <adrian.mihu20@icloud.com>
2025-11-15 03:32:49 +0200

    Essentia Vial item registered and added models and textures

--- src/main/java/ro/maleficent/essentia/Essentia.java
+++ src/main/java/ro/maleficent/essentia/Essentia.java
@@ -2,16 +2,12 @@ package ro.maleficent.essentia;
 
 import net.fabricmc.api.ModInitializer;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Essentia implements ModInitializer {
 	public static final String MOD_ID = "essentia";
 
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
 	@Override
 	public void onInitialize() {
-
+        ModItems.register();
 	}
 }
---
+++ src/main/java/ro/maleficent/essentia/ModItems.java
@@ -0,0 +1,21 @@
+package ro.maleficent.essentia;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.item.Items;
+
+public class ModItems {
+    public static final RegistryKey<Item> ESSENTIAL_VIAL_KEY = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("essentia","essentia_vial"));
+
+    public static final Item ESSENTIA_VIAL = Items.register(ESSENTIAL_VIAL_KEY, Item::new, new Item.Settings().maxCount(1));
+
+    public static void register(){
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries ->{
+            entries.add(ESSENTIA_VIAL);
+        });
+    }
+}
